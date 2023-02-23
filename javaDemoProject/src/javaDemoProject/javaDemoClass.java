8e442a7366420e598f336376674ac0e40f719706
Janhavi Soni <jsoni@BLR.ALLEGISINDIA.COM>
2023-02-23 11:56:30 +0530

    Code

---
+++ javaDemoProject/src/javaDemoProject/javaDemoClass.java
@@ -0,0 +1,11 @@
+package javaDemoProject;
+
+public class javaDemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Coding in process");
+
+	}
+
+}
